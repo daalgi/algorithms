acb3ba3c6beee4be28306cdd34fe5748a3c86cad
daalgi <daalgicam@gmail.com>
2021-01-30 18:53:03 +0100

    .

--- maths/decimal_to_binary.py
+++ maths/decimal_to_binary.py
@@ -21,9 +21,22 @@ def max_number_of_consecutive_ones(binary: str):
 
 
 if __name__ == "__main__":
+    print('-' * 60)
+    print('Maximum number of consecutive ones in a binary number')
+    print('-' * 60)
     numbers = list(range(10)) + list(range(20,200,20))
     for n in numbers:
         b = decimal_to_binary(n)
         res = f'{" " * (6-len(str(n)))}({n})_10 = ({b})_2{" " * (10-len(b))}'
         res += f' --> max consecutive ones = {max_number_of_consecutive_ones(b)}'
         print(res)
+
+    print()
+    print('-' * 60)
+    print('Convert decimal numbers to binary')
+    print('-' * 60)
+    while True:
+        # decimal number
+        number = int(input("Enter any decimal number:  "))
+
+        print('    -> Binary equivalent: ', decimal_to_binary(number))
