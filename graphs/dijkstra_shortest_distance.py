b570fd93e03271f8604667842c00898f9aeeaedc
daalgi <daalgicam@gmail.com>
2021-02-17 21:46:35 +0100

    efficiency fix and added tests

--- graphs/dijkstra_shortest_distance.py
+++ graphs/dijkstra_shortest_distance.py
@@ -14,22 +14,31 @@ class Edge:
 
 
 def dijkstra_shortest_distance(graph: dict, start: int, verbose: bool = False):
+    out = ''
     n = len(graph)
+    # Visited nodes array to avoid re-visiting nodes
+    visited = [False] * n
+    # Keep track of the minimum distance to each node
     dist = [float('Inf')] * n
     dist[start] = 0
+    # PriorityQueue of the next most promising node to visit (shortest distance)
     pq = queue.PriorityQueue()
-    pq.put((start, 0))
-    out = ''
+    pq.put((0, start))   
+
     while not pq.empty():
-        node, min_val = pq.get()
+        min_val, node = pq.get()
         
         if verbose:
             out = f'Current node {node} distance: {min_val}'
         
+        visited[node] = True
+
         if dist[node] < min_val: continue
 
         for edge in graph[node]:
             
+            if visited[edge.to]: continue
+
             new_dist = dist[node] + edge.cost
 
             if verbose:
@@ -41,13 +50,14 @@ def dijkstra_shortest_distance(graph: dict, start: int, verbose: bool = False):
                     out += f'\t--> Node {edge.to} distance updated!'
                 
                 dist[edge.to] = new_dist
-                pq.put((edge.to, new_dist))
+                # Insert tuple (dist, node) so they're sorted by dist
+                pq.put((new_dist, edge.to))
 
         if verbose:
-            print(out) 
+            print(out)
 
     if verbose:
-        print(f'>> Distance array: {str(dist)}\n')
+        print(f'>> Distance array: {str(dist)}')
     return dist
 
 
@@ -63,17 +73,25 @@ if __name__ == '__main__':
             2: [Edge(1, 2), Edge(3, 5)],
             3: [Edge(4, 3)],
             4: []
-        }, 
+        }, { 
+            0: [Edge(1, 5), Edge(2, 1)],
+            1: [Edge(2, 2), Edge(3, 3), Edge(4, 20)],
+            2: [Edge(1, 3), Edge(4, 12)],
+            3: [Edge(4, 2), Edge(5, 6)],
+            4: [Edge(5, 1)],
+            5: []
+        }
     ]
 
     testcases = [
         (graphs[0], 0, [0, 3, 1, 4, 7]),
+        (graphs[1], 0, [0, 4, 1, 7, 9, 10])
     ]
 
     for graph, start, solution in testcases:
         print(f'\nGraph: {graph}')
         dist = dijkstra_shortest_distance(graph, start, True)
-        print(f'Distances: {dist}')
+        # print(f'Distances: {dist}')
         if solution:
             print(f'Test: {"OK" if dist == solution else "NOT OK"}\n')
         print()
--- graphs/dijkstra_shortest_path.py
+++ graphs/dijkstra_shortest_path.py
@@ -15,27 +15,34 @@ class Edge:
 
 def dijkstra(graph: dict, start: int, verbose: bool = False):
     n = len(graph)
+    # Visited nodes array to avoid re-visiting nodes
+    visited = [False] * n
+    # Keep track of the minimum distance to each node
     dist = [float('Inf')] * n
-    dist[start] = 0
+    dist[start] = 0    
+    # PriorityQueue of the next most promising node to visit (shortest distance)
     pq = queue.PriorityQueue()
-    pq.put((start, 0))
-
+    pq.put((0, start))
     # array to track the node you took to get to the i-th node
     prev = [None] * n
 
     while not pq.empty():
-        node, min_val = pq.get()
-            
+        min_val, node = pq.get()
+        
+        visited[node] = True
+
         if dist[node] < min_val: continue
 
         for edge in graph[node]:
             
+            if visited[edge.to]: continue
+
             new_dist = dist[node] + edge.cost
     
             if new_dist < dist[edge.to]:
                 prev[edge.to] = node
                 dist[edge.to] = new_dist
-                pq.put((edge.to, new_dist))
+                pq.put((new_dist, edge.to))
 
     return (dist, prev)
 
@@ -81,14 +88,22 @@ if __name__ == '__main__':
             2: [Edge(1, 2), Edge(3, 5)],
             3: [Edge(4, 3)],
             4: []
-        }, 
+        }, { 
+            0: [Edge(1, 5), Edge(2, 1)],
+            1: [Edge(2, 2), Edge(3, 3), Edge(4, 20)],
+            2: [Edge(1, 3), Edge(4, 12)],
+            3: [Edge(4, 2), Edge(5, 6)],
+            4: [Edge(5, 1)],
+            5: []
+        }
     ]
 
     testcases = [
         (graphs[0], 0, 4, [0, 2, 1, 3, 4]),
         (graphs[0], 0, 3, [0, 2, 1, 3]),
-        (graphs[0], 1, 3, [1, 3]),
         (graphs[0], 4, 3, []),
+        (graphs[1], 1, 5, [1, 3, 4, 5]),
+        (graphs[1], 0, 3, [0, 2, 1, 3]),
     ]
 
     for graph, start, end, solution in testcases:
