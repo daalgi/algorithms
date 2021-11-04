35a6235a0d82f32568410ba1ceb137d1e26aa6fb
daalgi <daalgicam@gmail.com>
2021-11-04 22:32:58 +0100

    .

--- trees/check_subtree.py
+++ trees/check_subtree.py
@@ -7,6 +7,29 @@ Create an algorithm to determine if T2 is a subtree of T1.
 A tree T2 is a subtree of T1 if there exists a node in T1 such that
 the subtree of n is identical to T2. That is, if you cut off the tree at
 node n, the two trees would be identical.
+
+https://leetcode.com/problems/subtree-of-another-tree/
+Given the roots of two binary trees root and subRoot, return true 
+if there is a subtree of root with the same structure and node 
+values of subRoot and false otherwise.
+
+A subtree of a binary tree tree is a tree that consists of a node 
+in tree and all of this node's descendants. The tree tree could 
+also be considered as a subtree of itself.
+
+Example 1:
+Input: root = [3,4,5,1,2], subRoot = [4,1,2]
+Output: true
+
+Example 2:
+Input: root = [3,4,5,1,2,null,null,null,null,0], subRoot = [4,1,2]
+Output: false
+
+Constraints:
+The number of nodes in the root tree is in the range [1, 2000].
+The number of nodes in the subRoot tree is in the range [1, 1000].
+-104 <= root.val <= 104
+-104 <= subRoot.val <= 104
 """
 from collections import deque
 
@@ -22,37 +45,37 @@ from tree_node import (
 )
 
 
-def recursion(root1: TreeNode, root2: TreeNode):
-    def f(root1: TreeNode, root2: TreeNode, node2: TreeNode = None):
-
-        # For convenience, if `node2` not passed and `root2` is a node,
-        # force `node2 = root2`
-        if root2 and not node2:
-            node2 = root2
-
-        # Base cases
-        if not root1 and not node2:
-            # If both trees have reached the leaves,
-            # they are at least partially equal
-            return True
-        if not root1 or not node2:
-            # If only one tree has reached a leaf, not equal
+def recursion(root1: TreeNode, root2: TreeNode) -> bool:
+    def is_subtree(r1: TreeNode, r2: TreeNode) -> bool:
+        if not r1:
+            # Empty "big tree"
             return False
 
-        # Recursive calls
-        if root1.data == root2.data:
-            # If booth `node.data` are equal, continue the
-            # comparison with the children
-            return f(root1.left, root2, node2.left) and f(
-                root1.right, root2, node2.right
-            )
+        if r1.data == r2.data and match_tree(r1, r2):
+            return True
 
-        # If not equal, move on to the children of `root1` and go back
-        # to the root of the second tree `root2`
-        return f(root1.left, root2) or f(root1.right, root2)
+        return is_subtree(r1.left, r2) or is_subtree(r1.right, r2)
 
-    # Call the recursive function
-    return f(root1, root2, node2=root2)
+    def match_tree(r1: TreeNode, r2: TreeNode) -> bool:
+        if not r1 and not r2:
+            # Both trees reached the leaves
+            return True
+        if not r1 or not r2:
+            # One tree reached the leaf while the other didn't
+            return True
+        if r1.data != r2.data:
+            # Data doesn't match
+            return False
+        # Recursive call to check children
+        return match_tree(r1.left, r2.left) and match_tree(r1.right, r2.right)
+
+    # Before starting, check a base case
+    if not root2:
+        # An empty tree is always a subtree
+        return True
+
+    # Start recursive algorithm
+    return is_subtree(root1, root2)
 
 
 if __name__ == "__main__":
@@ -61,7 +84,16 @@ if __name__ == "__main__":
     print("-" * 60)
 
     test_cases = [
+        ([1, 2, 3, 4, 5, 6, 7, 8], [1], True),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [3], True),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [5], True),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [7, 8], True),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [6, 5, 7, 8], True),
         ([1, 2, 3, 4, 5, 6, 7, 8], [3, 4, 5], False),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [4, 6, 2], False),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [3, 2, 1], False),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [2, 3, 1], True),
+        ([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 3], True),
     ]
 
     print("\n>>>>> Binary Tree:")
@@ -70,14 +102,14 @@ if __name__ == "__main__":
     root = sorted_array_to_bst(arr)
     printTree(root)
 
-    for nums1, nums2, solution in test_cases[:3]:
+    for nums1, nums2, solution in test_cases:
 
         # TODO implement pre-order traversal for creation of tree from list
         root1 = create_binary_tree_from_list(nums1)
         root2 = create_binary_tree_from_list(nums2)
         result = recursion(root1, root2)
 
-        string = f"recursion{nums1, nums2} = "
+        string = f"recursion({nums2}) = "
         string += " " * (35 - len(string))
         string += str(result)
         string += " " * (60 - len(string))
