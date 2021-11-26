ab1f6ef63d764247764a8e03c8478fe41aa53ac5
daalgi <daalgicam@gmail.com>
2021-11-26 23:58:33 +0100

    .

--- bit-manipulation/next_number.py
+++ bit-manipulation/next_number.py
@@ -28,6 +28,68 @@ def brute_force(num: int) -> int:
     return res[0]
 
 
+def next_numbers(num: int) -> int:
+    # Temporary variable to perform shifts on the original
+    # value of `num`
+    temp = num
+
+    # Number of trailing zeroes
+    zeroes = 0
+
+    # Loop until the rightmost one
+    while temp & 1 == 0:
+        zeroes += 1
+        temp >>= 1
+
+    # Number of ones until the rightmost non-trailing zero
+    ones = 0
+
+    # Loop until the rightmost non-trailing zero
+    while temp & 1:
+        ones += 1
+        temp >>= 1
+
+    # Position where a 0-bit will be flipped into a 1-bit,
+    # that is the rightmost non-trailing zero
+    p = zeroes + ones
+
+    # print(p, ones, zeroes)
+    # print(bin(num))
+
+    # Flip the rightmost non-trailing zero
+    num |= 1 << p
+    # print(bin(num))
+
+    # # All zeros except for a 1 at position p
+    # a = 1 << p
+    # # All zeros, followed by p ones
+    # b = a - 1
+    # # All ones, followed by p zeros
+    # mask = ~b
+    # # Clear rightmost p bits
+    # num &= mask
+
+    # Which is equivalent to:
+    # All zeros to the right of `p`
+    num &= ~((1 << p) - 1)
+    # print(bin(num))
+
+    # Add the counted number of 1s, minus 1, at the right
+    # # 0s with a 1 at position ones - 1
+    # a = 1 << (ones - 1)
+    # # 0s with 1s at positions 0 through `ones - 1`
+    # b = a - 1
+    # # Insert 1s at positions 0 through `ones - 1`
+    # num |= b
+
+    # which is equivalent to:
+    # `ones - 1` 1s at the right
+    num |= (1 << (ones - 1)) - 1
+    # print(bin(num))
+
+    return num
+
+
 if __name__ == "__main__":
     print("-" * 60)
     print("Next number")
@@ -48,18 +110,18 @@ if __name__ == "__main__":
 
     for num, solution in test_cases:
 
-        string = f"brute_force({num}) = "
+        string = f" brute_force({num}) = "
         string += " " * (25 - len(string))
         result = brute_force(num)
         string += str(result)
         string += " " * (60 - len(string))
         print(string, f'\t\tTest: {"OK" if solution == result else "NOT OK"}')
 
-        # string = f"next_numbers({num}) = "
-        # string += " " * (25 - len(string))
-        # result = next_numbers(num)
-        # string += str(result)
-        # string += " " * (60 - len(string))
-        # print(string, f'\t\tTest: {"OK" if solution == result else "NOT OK"}')
+        string = f"next_numbers({num}) = "
+        string += " " * (25 - len(string))
+        result = next_numbers(num)
+        string += str(result)
+        string += " " * (60 - len(string))
+        print(string, f'\t\tTest: {"OK" if solution == result else "NOT OK"}')
 
         print()
