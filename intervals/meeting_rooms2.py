ab157b250c13943f3755ec737d3fefdf5a7b4983
daalgi <daalgicam@gmail.com>
2021-12-23 22:16:51 +0100

    .

--- intervals/meeting_rooms2.py
+++ intervals/meeting_rooms2.py
@@ -1,7 +1,6 @@
 """
 https://www.lintcode.com/problem/919/
 
-
 Given an array of meeting time intervals consisting of start and 
 end times [[s1,e1],[s2,e2],…] (si < ei), find the minimum number 
 of conference rooms required.
