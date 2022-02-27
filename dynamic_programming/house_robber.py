0819f33c7f96d859b8a62645cada17940a62f518
daalgi <daalgicam@gmail.com>
2022-02-27 21:45:55 +0100

    .

--- dynamic_programming/house_robber.py
+++ dynamic_programming/house_robber.py
@@ -30,14 +30,15 @@ Constraints:
 1 <= nums.length <= 100
 0 <= nums[i] <= 400
 """
+from typing import List
 import time
 from random import randint
 
 
-def bf_recursion(nums: list) -> int:
+def brute_force(nums: List[int]) -> int:
     # Brute force with recursion
     # Time complexity: O(2^n)
-    # We have two choices for each house (rob or not): 
+    # We have two choices for each house (rob or not):
     # 2 * 2 * ... = 2^n
     # Space complexity: O(n)
     # Max depth of the stack: n
@@ -54,16 +55,13 @@ def bf_recursion(nums: list) -> int:
         # Max of:
         # - don't rob the current house, go for the next one
         # - rob the current house, skip the next one
-        return max(
-            recursion(house + 1), 
-            nums[house] + recursion(house + 2)
-        )
+        return max(recursion(house + 1), nums[house] + recursion(house + 2))
 
     # Call the recursive function starting from the house 0
     return recursion(0)
 
 
-def dp_recursion(nums: list) -> int:
+def dp_recursion(nums: List[int]) -> int:
     # Dynamic programming with memoization (recursive)
     # Time complexity: O(n)
     # Space complexity: O(n)
@@ -79,23 +77,20 @@ def dp_recursion(nums: list) -> int:
         # Base case
         if house >= houses:
             return 0
-        
+
         # Check if the result has been already computed
         if memo[house] is not None:
             return memo[house]
 
         # Max of the current house
-        memo[house] = max(
-            recursion(house + 1),
-            nums[house] + recursion(house + 2)
-        )
+        memo[house] = max(recursion(house + 1), nums[house] + recursion(house + 2))
         return memo[house]
 
     # Call the recursive function starting at house 0
     return recursion(0)
 
 
-def dp_iter(nums: list) -> int:
+def dp_iter(nums: List[int]) -> int:
     # Dynamic programming with tabulation (iterative)
     # Time complexity: O(n)
     # Space complexity: O(n)
@@ -106,23 +101,37 @@ def dp_iter(nums: list) -> int:
         return nums[0]
 
     # Table to store the results from the bottom up.
-    # Each element represents the maximum robbed possible 
+    # Each element represents the maximum robbed possible
     # value considering the previous houses
-    table = [*nums]
-    table[1] = max(nums[0], nums[1])
+    table = [0] * (houses + 1)
+    table[1] = nums[0]
 
     # Loop over the houses
-    for house in range(2, houses):
+    for house in range(1, houses):
         # For the current house, take the maximum considering:
         # - not robbing the current house, but the prev one
         # - robbing the current house and the prev to the prev one
-        table[house] = max(
-            table[house - 1], 
-            nums[house] + table[house - 2]
-        )
+        table[house + 1] = max(table[house], nums[house] + table[house - 1])
     # Return the value of the last index in the table, which
     # denotes the maximum value for all the houses
     return table[-1]
+
+
+def dp_iter_opt(nums: List[int]) -> int:
+    # Dynamic programming with tabulation (iterative)
+    # Time complexity: O(n)
+    # Space complexity: O(1)
+
+    houses = len(nums)
+
+    if houses == 1:
+        return nums[0]
+
+    prev1 = prev2 = 0
+    for house in range(houses):
+        prev1, prev2 = max(prev2 + nums[house], prev1), prev1
+
+    return prev1
 
 
 if __name__ == "__main__":
@@ -140,28 +149,31 @@ if __name__ == "__main__":
 
     for nums, solution in test_cases:
 
-        print(nums)
-        
-        result = bf_recursion(nums)
-        string = f" bf_recursion = "
-        string += " " * (25 - len(string))
-        string += str(result)
-        string += " " * (60 - len(string))
-        print(string, f'\t\tTest: {"OK" if solution == result else "NOT OK"}')
+        print("Nums:", nums)
+
+        result = brute_force(nums)
+        output = f"     brute_force = "
+        output += str(result)
+        output += " " * (60 - len(output))
+        print(output, f'Test: {"OK" if solution == result else "NOT OK"}')
 
         result = dp_recursion(nums)
-        string = f" dp_recursion = "
-        string += " " * (25 - len(string))
-        string += str(result)
-        string += " " * (60 - len(string))
-        print(string, f'\t\tTest: {"OK" if solution == result else "NOT OK"}')
+        output = f"    dp_recursion = "
+        output += str(result)
+        output += " " * (60 - len(output))
+        print(output, f'Test: {"OK" if solution == result else "NOT OK"}')
 
         result = dp_iter(nums)
-        string = f"      dp_iter = "
-        string += " " * (25 - len(string))
-        string += str(result)
-        string += " " * (60 - len(string))
-        print(string, f'\t\tTest: {"OK" if solution == result else "NOT OK"}')
+        output = f"         dp_iter = "
+        output += str(result)
+        output += " " * (60 - len(output))
+        print(output, f'Test: {"OK" if solution == result else "NOT OK"}')
+
+        result = dp_iter_opt(nums)
+        output = f"     dp_iter_opt = "
+        output += str(result)
+        output += " " * (60 - len(output))
+        print(output, f'Test: {"OK" if solution == result else "NOT OK"}')
 
         print()
 
@@ -170,14 +182,14 @@ print("\n\n>>> Performance tests:")
 for size in [30, 500, 20000]:
 
     nums = [randint(0, 100) for _ in range(size)]
-    print('\n>Array size:', size)
+    print("\n>Array size:", size)
 
     if size < 40:
         t = time.time()
-        print(f"\n Brute force: f = {bf_recursion(nums)}")
+        print(f"\n Brute force: f = {brute_force(nums)}")
         t = time.time() - t
         print(f"Computation time: {t*1e3:>8.0f} ms")
-    
+
     if size < 1000:
         t = time.time()
         print(f"\nDp recursion: f = {dp_recursion(nums)}")
